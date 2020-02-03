8f1eb8c25b0ae16b51e767354603541c79f823bc
marville001 <mwangimartin1904@gmail.com>
2020-02-03 08:26:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+//
+
+const app = new Clarifai.App({
+  apiKey: "b881c277bc80412cbd14e42818b7e74f"
+});
+//
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+//
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imgUrl: "",
+      box: {}
+    };
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+    //
+  };
+  displayFaceBox = box => {
+    this.setState({ box });
+  };
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  //
+  onButtonSubmit = () => {
+    this.setState(() => {
+      return { imgUrl: this.state.input };
+    });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
+
+  //
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imgUrl={this.state.imgUrl} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Signin = () => {
+  return (
+    <article className="br2 ba dark-gray-10 mv4 ">
+      <main className="pa4 black-80">
+        <form className="measure center">
+          <fieldset className="" id="sign_up"></fieldset>
+        </form>
+      </main>
+    </article>
+  );
+};
+
+export default Signin;
